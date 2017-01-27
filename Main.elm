ba5a49f17ee7e6f8b5c2ffcc6c0ef8b0ba467e92
Darwin Wu <dwzigzag@gmail.com>
2017-01-27 02:27:52 -0800

    got new line working

--- Main.elm
+++ Main.elm
@@ -1,8 +1,73 @@
-module Memorizer exposing (..)
-
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 import String
+import List
+import Regex exposing (..)
+
 
 
 main =
-    text "Hello"
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { content : String
+  }
+
+
+model : Model
+model =
+  Model ""
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Change String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+firstLetters : String -> String
+firstLetters =
+  replace All (regex "\\B\\w") (\_ -> "_")
+
+firstWords : String -> String
+firstWords =
+  replace All (regex "\\s(.*)") (\_ -> "\n")
+
+newLineHandler : String -> List (Html msg)
+newLineHandler textBox =
+  textBox
+    |> String.split "\n"
+    |> List.map Html.text
+    |> List.intersperse ( br [] [] )
+
+-- VIEW
+
+place = """Text to \n
+filter"""
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ textarea [ placeholder "Fill this \n in", onInput Change ] []
+    , div [] [ text "Hello",  br [] [], text "World" ]
+    , div [] [ text (firstLetters model.content) ]
+    , div [] [ text (firstWords model.content) ]
+    , div [] ( newLineHandler model.content )
+    ]
